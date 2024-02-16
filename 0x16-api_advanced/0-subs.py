faeb99e0bad2a01552f3ecd6d686b591e103e8aa
Aden1ke <sarahogunlalu@.com>
2024-02-16 05:41:14 +0100

    prints the titles of the first 10 hot posts listed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    class for number of subscribers for a given subreddit
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyBot/0.0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -16,4 +19,3 @@ def number_of_subscribers(subreddit):
         return data['data']['subscribers']
     else:
         return 0
-
